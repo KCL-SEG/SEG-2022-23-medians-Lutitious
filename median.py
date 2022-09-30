f6ecf26b6eb3d902dd035b08a38c3f5db08f07cb
Rahul Imran <notlutitious@gmail.com>
2022-09-30 20:30:07 +0100

    Feat: Add support for outputting the median number

--- median.py
+++ median.py
@@ -5,6 +5,12 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        if len(numbers) % 2 == 0:
+            median = (numbers[len(numbers)//2] + numbers[len(numbers)//2 - 1])/2
+        else:
+            median = numbers[len(numbers)//2]
+        print("The median is: ", median)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
